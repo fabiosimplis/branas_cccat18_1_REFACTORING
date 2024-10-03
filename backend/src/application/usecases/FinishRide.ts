7de2af1e7ae8b1656f707295f5f8066059158aa5
fabiosimplis <fabio.simplis@gmail.com>
2024-10-03 11:28:17 -0300

    ADD Use case call other Use Case

--- backend/src/application/usecases/FinishRide.ts
+++ backend/src/application/usecases/FinishRide.ts
@@ -2,6 +2,8 @@ import Position from "../../domain/vo/Position";
 import { inject } from "../../infra/DI/DI";
 import PositionRepository from "../../infra/Repository/PositionRepository";
 import RideRepository from "../../infra/Repository/RideRepository";
+import GenerateInvoice from "./GenerateInvoice";
+import ProcessPayment from "./ProcessPayment";
 
 //Use Case
 export default class FinishRide{
@@ -18,6 +20,12 @@ export default class FinishRide{
     const positions = await this.positionRepository?.getPositionsByRideId(input.rideId);
     ride.finish(positions);
     await this.rideRepository.updateRide(ride);
+    // chamando outro use case
+    const processPayment = new ProcessPayment();
+    await processPayment.execute({rideId: ride.getRideId(), amount: ride.getFare()})
+    // chamando outro use case 
+    const generateInvoice = new GenerateInvoice();
+    await generateInvoice.execute({rideId: ride.getRideId(), amount: ride.getFare()})
   }
 }
 
